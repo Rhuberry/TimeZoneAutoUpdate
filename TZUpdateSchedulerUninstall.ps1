7bbb053eef5d6f410088d9e1ce77fe6b4c2995fc
Rhuberry <143249848+Rhuberry@users.noreply.github.com>
2026-01-19 18:33:52 -0600

    Update TZUpdateSchedulerUninstall.ps1

--- TZUpdateSchedulerUninstall.ps1
+++ TZUpdateSchedulerUninstall.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = "SilentlyContinue"
 
 $baseTaskName = "Time Zone Update"
 $taskHourly   = "$baseTaskName (Hourly)"
-$taskLogon    = "$baseTaskName (Logon)"
+$taskSignIn   = "$baseTaskName (SignIn Event)"
 
 $scriptDir = "C:\ProgramData\TimeZoneTaskScheduler"
 $regPath   = "HKLM:\SOFTWARE\TimeZoneTaskScheduler"
 
 # Remove scheduled tasks
 schtasks /Delete /TN "$taskHourly" /F | Out-Null
-schtasks /Delete /TN "$taskLogon" /F | Out-Null
+schtasks /Delete /TN "$taskSignIn" /F | Out-Null
 
 # Remove script folder
 if (Test-Path $scriptDir) {
